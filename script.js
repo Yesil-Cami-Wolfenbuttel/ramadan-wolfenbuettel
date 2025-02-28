72179f16a5f462598981fea3a8bbdb54374dac35
cihatsuel <cihat.suel55@hotmail.de>
2025-03-01 00:15:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,9 @@ function formatDate(date) {
 
 function loadPrayerTimes() {
   const now = new Date();
-  const today = now.toISOString().split('T')[0];
+const today = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
+
+
 
   const todaysTimes = prayerTimes.find(pt => pt.date === today);
   const nextDay = new Date(now);
